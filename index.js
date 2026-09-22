5ea6d2c2f3f9a88a79ee47ed79dc6a49ed334e21
Jorge Saavedra Castillo <jsaavedra@Mac.home>
2026-09-22 20:49:39 +0200

    actualizacion controller, routes and services

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Función con anotaciones de tipo para persona y fecha
-function saludar(persona, fecha) {
-    console.log(`¡Hola ${persona}! Hoy es ${fecha.toDateString()}`);
-}
-saludar("Desarrollador", new Date());
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,6 +0,0 @@
-// Función con anotaciones de tipo para persona y fecha
-function saludar(persona: string, fecha: Date) {
-  console.log(`¡Hola ${persona}! Hoy es ${fecha.toDateString()}`);
-}
-
-saludar("Desarrollador", new Date());
